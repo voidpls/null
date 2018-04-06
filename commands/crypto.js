c49df52f0cec4e394525b4441ec58c166d361904
MacBook <raze2736@gmail.com>
2018-04-05 23:50:49 -0400

    crypto

---
+++ commands/crypto.js
@@ -0,0 +1,66 @@
+const Discord = require("discord.js");
+//const errors = require("../utils/errors.js");
+const config = require("../config/config.json");
+const cc = require('cryptocompare');
+
+module.exports.run = async (bot, msg, args, prefix) => {
+
+  if (args.length == 0) {
+
+    cc.priceMulti(['BTC', 'BCH', 'ETH', 'LTC', 'XRP', 'EOS'], 'USD')
+    .then(prices => {
+
+      let embed = new Discord.RichEmbed()
+      .setAuthor('Cryptocurrency Values', 'https://cdn-images-1.medium.com/max/1600/1*U7phpu7aKKrU05JvMvs-wA.png')
+      .setColor(config.colors.white)
+      .setFooter(`Use ${prefix}crypto [crypto symbol] [currency] to get custom results`);
+
+      for (i in prices) {
+        embed.addField(i, '$'+prices[i]['USD'], true)
+      }
+
+      msg.channel.send(embed)
+
+    })
+
+  }
+
+  else {
+
+    let coin = args[0].toUpperCase();
+    let currency = args[1] || 'USD';
+    let coinlist = await cc.coinList();
+    cc.priceFull(coin, currency.toUpperCase()).then(prices => {
+
+      let cData = coinlist.Data[coin]
+      let pData = prices[coin][currency.toUpperCase()]
+
+      let embed = new Discord.RichEmbed()
+      .setAuthor(cData.FullName, `https://www.cryptocompare.com${cData.ImageUrl}`)
+      .setColor(config.colors.white)
+      .addField(`Price (${currency})`, pData.PRICE)
+      .addField(`Market Cap (${currency})`, Math.round(pData.MKTCAP).toLocaleString())
+      .addField(`% Change (24h)`, Math.round(pData.CHANGEPCT24HOUR*10)/10+'%')
+      .addField(`Supply`, Math.round(pData.SUPPLY).toLocaleString())
+      .setFooter(`Last Market: ${pData.LASTMARKET}`)
+
+      msg.channel.send(embed);
+      
+    }).catch(e => {
+      console.log(e)
+      return msg.channel.send(`<:error:335660275481051136> Could not find coin data for **${coin}** in **${currency.toUpperCase()}**`);
+    });
+
+  }
+
+
+
+};
+
+module.exports.help = {
+  name: "crypto",
+  desc: "Checks the worth of any crypto, in any currency.",
+  usage: "crypto [crypto symbol] [currency]",
+  category: 'Utilities',
+  aliases: ['cryptocurrency', 'cc']
+}
--- utils/errors.js
+++ utils/errors.js
@@ -4,42 +4,56 @@ const fs = require("fs");
 //No perms error
 module.exports.noPerms = async (msg, perm) => {
 
-    await msg.channel.send(`<:error:335660275481051136> You need **${perm}** to use this command`).then(m => m.delete(5000));
+    await msg.channel.send(`<:error:335660275481051136> You need the **${perm}** perm to use this command`)
+    .then(m => {m.delete(5000); msg.delete()});
 
 };
+
+module.exports.noPerms2 = async (msg, perm) => {
+
+    await msg.channel.send(`<:error:335660275481051136> I don't have the **${perm}** perm`)
+    .then(m => {m.delete(5000); msg.delete()});
+
+};
+
 
 //Command not found error
 module.exports.noCmd = async (msg, cmd) => {
 
-    await msg.channel.send(`<:error:335660275481051136> Command **${cmd}** not found`).then(m => m.delete(5000));
+    await msg.channel.send(`<:error:335660275481051136> Command **${cmd}** not found`).then(m => m.delete(5000))
+    .then(m => {m.delete(5000); msg.delete()});
 
 };
 
 //Definition not found error
 module.exports.notFound = async (msg, def) => {
 
-    await msg.channel.send(`<:error:335660275481051136> Definition for **${def}** not found`).then(m => m.delete(5000));
+    await msg.channel.send(`<:error:335660275481051136> Definition for **${def}** not found`).then(m => m.delete(5000))
+    .then(m => {m.delete(5000); msg.delete()});
 
 };
 
 //Weather data not found
 module.exports.weather = async (msg, loc) => {
 
-    await msg.channel.send(`**<:error:335660275481051136> Could not retreive weather data for \`${loc}\`**`);
+    await msg.channel.send(`**<:error:335660275481051136> Could not retreive weather data for \`${loc}\`**`)
+    .then(m => {m.delete(5000); msg.delete()});
 
 };
 
 //specify user
 module.exports.specifyUser = async (msg) => {
 
-    await msg.channel.send(`**<:error:335660275481051136> Please specify a username and platform.**`);
+    await msg.channel.send(`**<:error:335660275481051136> Please specify a username and platform.**`)
+    .then(m => {m.delete(5000); msg.delete()});
 
 };
 
 //player not found
 module.exports.noPlayer = async (msg, name) => {
 
-    await msg.channel.send(`**<:error:335660275481051136> Player ${name} not found**`);
+    await msg.channel.send(`**<:error:335660275481051136> Player ${name} not found**`)
+    .then(m => m.delete(5000));
 
 };
 
