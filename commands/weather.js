e6f8fbfd4d145f166970a397b05090715efd1c5e
MacBook <raze2736@gmail.com>
2018-04-29 00:26:16 -0400

    remove commented code

--- commands/weather.js
+++ commands/weather.js
@@ -34,13 +34,6 @@ module.exports.run = async (bot, msg, args, prefix) => {
     args.shift();
     let loc = args.join(' ')
 
-    /*let weatherData = JSON.parse(fs.readFileSync("./config/weather.json", "utf8"));
-    weatherData[msg.author.id] = args.join(' ');
-
-    fs.writeFile("./config/weather.json", JSON.stringify(weatherData, null, '\t'), (err) => {
-      if (err) return console.log(err);
-      else return msg.channel.send(`Your location has been successfully updated to **${weatherData[msg.author.id]}**`)
-    });*/
     let userloc = await Weather.findOne({ where: { userid: msg.author.id } })
     if (!userloc) {
       Weather.create({
@@ -55,8 +48,7 @@ module.exports.run = async (bot, msg, args, prefix) => {
 
   }
   else if (msg.mentions.users.size !== 0) {
-    /*let weatherData = JSON.parse(fs.readFileSync("./config/weather.json", "utf8"));
-    let loc = weatherData[msg.mentions.users.first().id]; */
+
     let mentionID = msg.mentions.users.first().id
 
     let loc = await Weather.findOne({ where: { userid: mentionID } })
@@ -65,10 +57,8 @@ module.exports.run = async (bot, msg, args, prefix) => {
     else return msg.channel.send(`No weather data found for user **${msg.mentions.users.first().username}**`)
   }
   else if (args.length === 0) {
-    /*let weatherData = JSON.parse(fs.readFileSync("./config/weather.json", "utf8"));
-    let loc = weatherData[msg.author.id];*/
+
     let loc = await Weather.findOne({ where: { userid: msg.author.id } })
-    console.log(loc)
     if (loc) return util.wSearch(msg, loc.get('location'));
     else msg.channel.send(`Type \`${prefix}weather set [location]\` to set a location.`);
   }
