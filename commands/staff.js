d19fe539065618e62bd0c90fe56f2df81727e197
MacBook <raze2736@gmail.com>
2018-05-18 23:18:26 -0400

    online staff

---
+++ commands/staff.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js')
+const config = require('../config/config.json')
+
+module.exports.run = async (bot, msg, args, prefix) => {
+
+  //msg.guild.fetchMembers()
+
+  let members = msg.guild.members
+
+  let staff = members.filter(m => m.hasPermission('MANAGE_ROLES', false, true, true))
+  let online = staff.filter(m => m.presence.status !== 'offline' && !m.user.bot).map(m => m.user.toString())
+  let offline = staff.filter(m => m.presence.status === 'offline' && !m.user.bot).map(m => `**${m.user.username}#${m.user.discriminator}**`)
+
+  let embed = new Discord.RichEmbed()
+  .setColor(config.colors.white)
+  .setDescription(
+    `List of **${msg.guild.name}** Staff:\n`+
+    `<:online:438877428807368705> ${online.join(' | ')}\n`+
+    `<:offline:438877460373831681> ${offline.join(' | ')}`
+  )
+  .setFooter(`Requested by ${msg.author.username}`)
+  .setTimestamp()
+
+  msg.channel.send(embed).catch(e => msg.channel.send(e.error))
+
+
+}
+
+module.exports.help = {
+  name: 'staff',
+  desc: "View the server's staff members",
+  usage: `staff`,
+  category: 'Info',
+  aliases: ['mods']
+
+}
