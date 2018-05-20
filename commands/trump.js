411260706c7fbb9cb1f7dc6765bdbc0a7ad8860d
MacBook <raze2736@gmail.com>
2018-05-20 17:34:05 -0400

    trump personalized

--- commands/trump.js
+++ commands/trump.js
@@ -1,10 +1,21 @@
 const axios = require('axios')
+const util = require('../utils/util.js')
 
 module.exports.run = async (bot, msg, args, prefix) => {
-  let trump = await axios.get(
-    'https://api.whatdoestrumpthink.com/api/v1/quotes/random'
+  let query = 'random'
+  if (args[0]) {
+    let user = util.getUserFromArg(bot, msg, args[0])
+
+    if (user) {
+      query = `personalized?q=**${user.username}**`
+    } else {
+      query = `personalized?q=**${args.join(' ')}**`
+    }
+  }
+  let res = await axios.get(
+    'https://api.whatdoestrumpthink.com/api/v1/quotes/' + query
   )
-  let quote = trump.data.message
+  let quote = res.data.message
   msg.channel
     .send(`"${quote}"\n\n*- Donald Trump*`)
     .catch(e => msg.channel.send(e.error))
@@ -12,8 +23,8 @@ module.exports.run = async (bot, msg, args, prefix) => {
 
 module.exports.help = {
   name: 'Trump',
-  desc: 'Gets a random Trump quote',
-  usage: 'trump',
+  desc: 'Gets a random Trump quote or generate a personalized one',
+  usage: 'trump [user]',
   category: 'Fun',
   aliases: ['donald']
 }
