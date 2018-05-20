b398c77d6db415116b6a6591744ad25a8f1f1769
MacBook <raze2736@gmail.com>
2018-05-20 18:07:21 -0400

    a

--- devCmds/eval.js
+++ devCmds/eval.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js')
 const util = require('../utils/util.js')
 const config = require('../config/config.json')
 
-let client = {'token': 'pranked, faggot'}
+let client = { token: 'pranked' }
 let gay = 'ur mom'
 
 module.exports.run = async (bot, msg, args, prefix) => {
@@ -10,8 +10,14 @@ module.exports.run = async (bot, msg, args, prefix) => {
     var code = args.join(' ')
     let evaled = eval(code)
 
-    if (typeof evaled !== 'string') { evaled = require('util').inspect(evaled) }
-    msg.channel.send('```xl\n' + util.clean(evaled) + '```').catch(e => msg.channel.send(`\`ERROR\` \`\`\`xl\n${util.clean(e)}\n\`\`\``))
+    if (typeof evaled !== 'string') {
+      evaled = require('util').inspect(evaled)
+    }
+    msg.channel
+      .send('```xl\n' + util.clean(evaled) + '```')
+      .catch(e =>
+        msg.channel.send(`\`ERROR\` \`\`\`xl\n${util.clean(e)}\n\`\`\``)
+      )
   } catch (err) {
     msg.channel.send(`\`ERROR\` \`\`\`xl\n${util.clean(err)}\n\`\`\``)
   }
