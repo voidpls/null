e71dd16932a528449a204a269b2699972f9db087
Win10@MacBook <raze2736@gmail.com>
2018-05-26 19:17:47 -0400

    a

--- commands/fortnite.js
+++ commands/fortnite.js
@@ -26,46 +26,50 @@ module.exports.run = async (bot, msg, args, prefix) => {
     username = args.join(' ')
   }
 
-  ftAPI.user(username, platform).then(data => {
-    let stats = data.stats.lifetime
+  ftAPI
+    .user(username, platform)
+    .then(data => {
+      let stats = data.stats.lifetime
 
-    let wins = fstat(stats, 'Wins')
-    let winPercent = fstat(stats, 'Win%')
-    let top3 = parseInt(fstat(stats, 'Top 3')) + parseInt(wins)
-    let kills = fstat(stats, 'Kills')
-    let KD = fstat(stats, 'K/d')
-    let mPlayed = fstat(stats, 'Matches Played')
+      let wins = fstat(stats, 'Wins')
+      let winPercent = fstat(stats, 'Win%')
+      let top3 = parseInt(fstat(stats, 'Top 3')) + parseInt(wins)
+      let kills = fstat(stats, 'Kills')
+      let KD = fstat(stats, 'K/d')
+      let mPlayed = fstat(stats, 'Matches Played')
 
-    let embed = new Discord.RichEmbed()
-      .setColor(config.colors.white)
-      .setTitle('Fortnite Stats')
-      .setAuthor(
-        data.platform.toUpperCase() + ' | ' + data.username,
-        'https://i.imgur.com/imPQal4.png'
-      )
-      .setThumbnail('https://i.imgur.com/da8iJVl.png')
-      .setFooter('Stats from FortniteTracker.com')
+      let embed = new Discord.RichEmbed()
+        .setColor(config.colors.white)
+        .setTitle('Fortnite Stats')
+        .setAuthor(
+          data.platform.toUpperCase() + ' | ' + data.username,
+          'https://i.imgur.com/imPQal4.png'
+        )
+        .setThumbnail('https://i.imgur.com/da8iJVl.png')
+        .setFooter('Stats from FortniteTracker.com')
 
-      .addField('Wins', wins, true)
-      .addField('Win %', winPercent, true)
-      .addField("Top 3's", top3, true)
-      .addField('Matches Played', mPlayed, true)
-      .addField('Kills', kills, true)
-      .addField('K/D', KD, true)
+        .addField('Wins', wins, true)
+        .addField('Win %', winPercent, true)
+        .addField("Top 3's", top3, true)
+        .addField('Matches Played', mPlayed, true)
+        .addField('Kills', kills, true)
+        .addField('K/D', KD, true)
 
-    msg.channel.send(embed).catch(e => {
-      console.log(e)
+      msg.channel.send(embed).catch(e => {
+        msg.channel.send(`**Error:** ${e.message}`)
+      })
+
+      function fstat(stats, str) {
+        return stats.find(s => s[str])[str]
+      }
+    })
+    .catch(e => {
       msg.channel
         .send(
           `**Error:** Player **${username}** on **${platform.toUpperCase()}** not found`
         )
         .then(m => m.delete(5000))
     })
-
-    function fstat(stats, str) {
-      return stats.find(s => s[str])[str]
-    }
-  })
 }
 
 module.exports.help = {
