1e26c8f9acf9d1afc10065a02f638858ad1641c3
voidpls <voidpls>
2018-06-22 14:36:29 -0400

    w

--- commands/weather.js
+++ commands/weather.js
@@ -84,14 +84,15 @@ async function wSearch(msg, loc) {
   let geoQ = `https://maps.googleapis.com/maps/api/geocode/json?key=${geocodeKey}&address=${loc}`
   let geocode = await axios.get(geoQ)
   if (geocode.status >= 400)
-    return msg.channel.send(geocode.statusText).catch(e => util.delCatch(e))
+    return msg.channel.send(`**Error:** Location not found.`)
   let lat = geocode.data.results[0].geometry.location.lat,
     long = geocode.data.results[0].geometry.location.lng,
     formatLoc = geocode.data.results[0].formatted_address
 
   let weatherQ = `https://api.darksky.net/forecast/${darkskyKey}/${lat},${long}?exclude=[minutely,alerts,flags]`
   let weather = await axios.get(weatherQ)
-
+  if (weather.status >= 400)
+    return msg.channel.send(`**Error:** Location not found.`)
   let currently = weather.data.currently,
     hourly = weather.data.hourly,
     daily = weather.data.daily.data[0]
@@ -134,7 +135,7 @@ async function wSearch(msg, loc) {
   msg.channel.send(embed).catch(e => msg.channel.send(`**Error:** ${e}`))
 }
 function toC(f) {
-  return Math.round((f - 32) * 5 / 9)
+  return Math.round(((f - 32) * 5) / 9)
 }
 
 module.exports.help = {
